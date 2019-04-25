edde9cf9687b0d410ec2518bf07c3e954690b113
Rick van der Wal <r.vanderwal@student.ru.nl>
2019-04-25 18:16:02 +0200

    Werkt bijna

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <map>
 
-enum hue {RED, YELLOW, GREEN, CYAN, BLUE, VIOLET, NONE};
+enum hue {RED, YELLOW, GREEN, CYAN, BLUE, VIOLET, NONE};    // LIGHT NONE is white and DARK NONE is black, NORMAL NONE is undefined
 enum lightness {LIGHT, NORMAL, DARK};
 enum direction {RIGHT, DOWN, LEFT, UP};
 enum chooser {LEFTMOST, RIGHTMOST};
@@ -14,28 +14,32 @@ struct color {
     lightness lightness;
 };
 
+struct block {
+    color color;
+    unsigned long size;
+    struct block* neighbors[8];
+};
+
+struct state {
+    block current;
+    std::stack<int> stack;
+    direction dp = RIGHT;
+    chooser cc = LEFTMOST;
+    uint8_t tries = 0;
+};
+
 struct position {
     int x;
     int y;
 };
 
-struct stain {
-    color color;
-    std::vector<position> coordinates;
-};
+bool compare_x(const position & p1, const position & p2) {
+    return p1.x < p2.x;
+}
 
-struct codel {
-    color color;
-    unsigned int size;
-    struct codel* neighbors[8];
-};
-
-struct state {
-    codel current;
-    std::stack<int> stack;
-    direction dp = RIGHT;
-    chooser cc = LEFTMOST;
-};
+bool compare_y(const position & p1, const position & p2) {
+    return p1.y < p2.y;
+}
 
 typedef void (*command) (state&);
 
@@ -141,7 +145,18 @@ void duplicate(state & state) {
 }
 
 void roll(state & state) {
+    int a = state.stack.top();
+    state.stack.pop();
 
+    int b = state.stack.top();
+    state.stack.pop();
+
+    for(int i = 0; i < a; i++) {
+        int n = state.stack.top();
+        state.stack.pop();
+
+        // Should probably finish this function
+    }
 }
 
 void in_number(state & state) {
@@ -176,8 +191,8 @@ void out_char(state & state) {
 
 const command commands[3][6] = {{skip, add, divide, greater, duplicate, in_char}, {push, subtract, mod, pointer, roll, out_number}, {pop, multiply, nott, switchh, in_number, out_char}};
 
-command get_command(const codel & from, const codel & to) {
-    if((from.color.hue == NONE and from.color.lightness == LIGHT) or (to.color.hue == NONE and to.color.lightness == LIGHT)) {  // Either from or to is white
+command get_command(const block & from, const block & to) {
+    if((from.color.hue == NONE and from.color.lightness == LIGHT) or (to.color.hue == NONE and to.color.lightness == LIGHT)) {  // Either going to or coming from a white color block
         return skip;
     }
 
@@ -188,13 +203,14 @@ command get_command(const codel & from, const codel & to) {
 }
 
 void next_state(state & state) {
-    codel next = *state.current.neighbors[state.dp * 4 + state.cc];
+    block next = *state.current.neighbors[state.dp * 4 + state.cc];
 
-    if(next.color.hue == NONE && next.color.lightness == DARK) {    // Bumped into black codel
-        state.dp = static_cast<direction>(state.dp + 1 % 4);
+    if(next.color.hue == NONE && next.color.lightness == DARK) {    // Bumped into black block
+        state.tries++;
+        if((state.tries & 1) == 0) state.dp = static_cast<direction>(state.dp + 1 % 4);
         state.cc = static_cast<chooser>(state.cc + 1 % 2);
     } else {
-        get_command(state.current, next) (state);       // Perform operation
+        get_command(state.current, next) (state);       // Perform operation associated with the color transition
         state.current = next;
     }
 }
@@ -223,7 +239,7 @@ void expand(const color (& colors)[6][15], bool (& done)[6][15], int x, int y, s
     }
 }
 
-codel find_codels(const std::string & image) {
+block find_blocks(const std::string &image) {
 
     // Read image
 
@@ -242,87 +258,99 @@ codel find_codels(const std::string & image) {
 
     bool done[width][height] = {false};
 
-    std::vector<stain> stains;
+    std::vector<block> blocks;
+    std::map<position, block> posToBlock;
+    std::map<block, std::vector<position>> blockToPos;
 
     for(int y = 0; y < height; y++) {
         for(int x = 0; x < width; x++) {
-            if(!done[x][y]) {
+            if(colors[x][y].hue == NONE and colors[x][y].lightness == LIGHT) {  // White pixel, should be a color block on its own
+                std::vector<position> positions;
+
+                positions.push_back({x, y});
+
+                done[x][y] = true;
+
+                block block = {colors[x][y], positions.size()};
+
+                blocks.push_back(block);
+
+                posToBlock[{x, y}] = block;
+                blockToPos[block] = positions;
+            } else if(!done[x][y]) {
                 std::vector<position> positions;
 
                 expand(colors, done, x, y, positions);
 
-                stain stain = {colors[x][y], positions};
-            }
-        }
-    }
+                block block = {colors[x][y], positions.size()};
 
-    // Transform color blocks to codels
+                blocks.push_back(block);
 
-    std::map<stain, codel> codelmap;
-    std::vector<codel> codels;
-
-    for(auto & stain : stains) {
-        codel codel = {stain.color, static_cast<unsigned int>(stain.coordinates.size())};
-        codelmap[stain] = codel;
-        codels.push_back(codel);
-    }
-
-    // Find neighboring codels
-
-    for(int i = 0; i < stains.size(); i++) {
-        stain stain = stains.at(i);
-
-        struct codel (& neighbors)[8];
-
-        // Right
-
-        std::vector<position> pixels;
-        pixels.push_back(stain.coordinates[0]);
-        for(int n = 1; n < stain.coordinates.size(); n++) {
-            int current = pixels[0].x;
-
-            if(current >= stain.coordinates[n].x) {
-                if(current != stain.coordinates[n].x) {
-                    pixels.clear();
+                for(auto position : positions) {
+                    posToBlock[position] = block;
                 }
-
-                pixels.push_back(stain.coordinates[n]);
+                blockToPos[block] = positions;
             }
         }
-
-        int min = pixels[0].y;
-        int max = pixels[0].y;
-        for(int n = 1; n < pixels.size(); n++) {
-            if(pixels[n].y < min) min = pixels[n].y;
-            if(pixels[n].y > max) max = pixels[n].y;
-        }
-
-        for(int n = 0; n < stains.size(); n++) {
-            if(std::find(stains[n].coordinates.begin(), stains[n].coordinates.end(), {pixels[0].x + 1, min}) != stains[n].coordinates.end()) {
-                neighbors[0] = & codelmap.at(stains[n]);
-            } else if(std::find(stains[n].coordinates.begin(), stains[n].coordinates.end(), {pixels[0].x + 1, max}) != stains[n].coordinates.end()) {
-                neighbors[1] = & codelmap.at(stains[n]);
-            }
-        }
-
-        // Down
-
-        // Left
-
-        // Up
-
-        codelmap.at(stain).neighbors = neighbors;
     }
 
-    // Return starting codel
+    // Find neighboring blocks
 
-    return codels[0];
+    for(auto block : blocks) {
+
+        struct block (& neighbors)[8];
+
+        std::vector<position> positions = blockToPos[block];
+
+        std::vector<position> right;
+        std::vector<position> down;
+        std::vector<position> left;
+        std::vector<position> up;
+
+        right.push_back(positions[0]);
+        down.push_back(positions[0]);
+        left.push_back(positions[0]);
+        up.push_back(positions[0]);
+
+        for(int i = 1; i < positions.size(); i++) {
+            position current = positions[i];
+
+            if(right[0].x <= current.x) {
+                if(right[0].x != current.x) right.clear();
+                right.push_back(current);
+            } else if(current.x <= left[0].x) {
+                if(left[0].x != current.x) left.clear();
+                left.push_back(current);
+            }
+
+            if(down[0].y <= current.y) {
+                if(down[0].y != current.y) down.clear();
+                down.push_back(current);
+            } else if(current.y <= up[0].y) {
+                if(up[0].y != current.y) up.clear();
+                up.push_back(current);
+            }
+        }
+
+        neighbors[0] = posToBlock.at(*std::min_element(right.begin(), right.end(), & compare_y));
+        neighbors[1] = posToBlock.at(*std::max_element(right.begin(), right.end(), & compare_y));
+        neighbors[2] = posToBlock.at(*std::max_element(down.begin(), down.end(), & compare_x));
+        neighbors[3] = posToBlock.at(*std::min_element(down.begin(), down.end(), & compare_x));
+        neighbors[4] = posToBlock.at(*std::max_element(left.begin(), left.end(), & compare_y));
+        neighbors[5] = posToBlock.at(*std::min_element(left.begin(), left.end(), & compare_y));
+        neighbors[6] = posToBlock.at(*std::min_element(up.begin(), up.end(), & compare_x));
+        neighbors[7] = posToBlock.at(*std::max_element(up.begin(), up.end(), & compare_x));
+    }
+
+    // Return starting block
+
+    return blocks[0];
 }
 
 int main() {
-    state state = {find_codels("lol")};
+    state state = {find_blocks("lol")};
 
-    while(true) {
+    while(state.tries < 8) {
         next_state(state);
     }
 
